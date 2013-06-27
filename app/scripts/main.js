7b82c4a29c0405d6e80aa9aea6d2c1fbb7d55bb4
Stuart Robinson <stumrob@gmail.com>
2013-06-28 00:26:10 +0100

    always display a cursor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,14 @@
 // var myApp = angular.module('myApp', []);
 
+// up    = function(){}
+// left  = function(){}
+// right = function(){}
+// down  = function(){}
 
 var $lettering = $('.lettering');
+		$cursor = $('i#cursor');
 
-var letterEl = function (letter) { return '<span class="current"><b>' + letter + '</b></span>' };
+var letterEl = function (letter) { return '<span><b>' + letter + '</b></span>' };
 
 var measureLetter = function (letter) {
 	if (letter == ' ') letter = 'I';
@@ -18,22 +23,19 @@ var insertion = {
 }
 
 window.onkeypress = function(k) {
-	if (insertion.$el && insertion.letter == String.fromCharCode(k.which)) {
-		return;
-	}
+	// repeated event
+	if (insertion.$el && insertion.letter == String.fromCharCode(k.which)) return;
 
-	insertion.startTime = Date.now();
-	insertion.letter = String.fromCharCode( k.which );
-	insertion.width = measureLetter( insertion.letter );
-	insertion.$el= $( letterEl( insertion.letter ) );
-	$('span').removeClass('current')
-	$lettering.append( insertion.$el )
+	insertion.startTime = Date.now()
+	insertion.letter = String.fromCharCode( k.which )
+	insertion.width = measureLetter( insertion.letter )
+	insertion.$el= $( letterEl( insertion.letter ) )
+	insertion.$el.insertBefore( $cursor  )
 };
 
 window.onkeydown = function(k) {
 	if (k.which == 8) {
 		$lettering.find('span').last().remove()
-		$lettering.find('span').last().addClass('current')
 		return false;
 	}
 }
